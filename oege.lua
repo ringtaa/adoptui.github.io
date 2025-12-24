2408450844f20e96a3b8b097f9ed5ededf1de698
ringtaa <eeeiqjjj50@gmail.com>
2025-12-24 12:16:10 -0500

    Create oege.lua

---
+++ oege.lua
@@ -0,0 +1,77 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local LocalPlayer = Players.LocalPlayer
+
+
+local Window = WindUI:CreateWindow({
+    Folder = "Ringta Scripts",
+    Title = "RINGTA",
+    Icon = "star",
+    Author = "discord.gg/ringta",
+    Theme = "Dark",
+    Size = UDim2.fromOffset(620, 460),
+    Transparent = false,
+    HasOutline = true,
+})
+
+Window:EditOpenButton({
+    Title = "Open RINGTA SCRIPTS",
+    Icon = "pointer",
+    CornerRadius = UDim.new(0, 6),
+    StrokeThickness = 2,
+    Color = ColorSequence.new(Color3.fromRGB(200, 0, 255), Color3.fromRGB(0, 200, 255)),
+    Draggable = true,
+})
+
+local Tabs = {
+    Main = Window:Tab({ Title = "Auto Neon", Icon = "lightbulb" }),
+    Hide = Window:Tab({ Title = "Baby Farm", Icon = "layers" }),
+    Jump = Window:Tab({ Title = "Auto Farm GingerBread", Icon = "bitcoin" }),
+    Glass = Window:Tab({ Title = "Pet Farm", Icon = "grid-2x2" }),
+    Mingle = Window:Tab({ Title = "Pet Spawner", Icon = "tent" }),
+}
+
+
+Tabs.Jump:Section({
+    Title = "Bypass",
+    Icon = "chess-queen",
+})
+
+Tabs.Jump:Toggle({
+    Title = "Enable Bypass Anticheat",
+    Default = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/NEWTPTRAIN.github.io/refs/heads/main/TRAIN.LUA"))()
+    end,
+})
+
+
+Tabs.Jump:Toggle({
+    Title = "Enable Fly",
+    Default = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/NEWTPTRAIN.github.io/refs/heads/main/TRAIN.LUA"))()
+    end,
+})
+
+
+Tabs.Jump:Section({
+    Title = "Best Features",
+    Icon = "medal",
+})
+
+Tabs.Jump:Button({
+    Title = "TP To StillWater Prison",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
+    end,
+})
+
+Tabs.Jump:Button({
+    Title = "Bring All Valuables Items",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
+    end,
+})
